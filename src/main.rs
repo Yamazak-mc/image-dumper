aaf14322ecb3d9de7e17f7af24c7aa84ab9bac5a
Yamazak-mc <50317858+Yamazak-mc@users.noreply.github.com>
2024-12-31 00:07:40 +0900

    add size report

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::HashMap,
-    io::{BufWriter, Write},
+    io::{BufReader, BufWriter, Read, Write},
     path::{Path, PathBuf},
     time,
 };
@@ -38,6 +38,8 @@ fn read_dir(dir: impl AsRef<Path>) -> Result<(), Box<dyn std::error::Error>> {
 
     let mut map = HashMap::new();
 
+    let mut size_before = 0;
+
     for entry in std::fs::read_dir(dir)? {
         let Ok(entry) = entry else {
             continue;
@@ -49,6 +51,13 @@ fn read_dir(dir: impl AsRef<Path>) -> Result<(), Box<dyn std::error::Error>> {
         if let Ok(img) = rdr.decode() {
             let img_as_bytes = img.to_rgba8().into_raw();
 
+            size_before += {
+                let mut buf = Vec::new();
+                let mut reader = BufReader::new(std::fs::File::open(entry.path())?);
+                reader.read_to_end(&mut buf)?;
+                buf.len()
+            };
+
             let mut buf = Vec::new();
             let mut encoder = ZlibEncoder::new(&mut buf, Compression::best());
             encoder.write_all(&img_as_bytes[..])?;
@@ -57,6 +66,10 @@ fn read_dir(dir: impl AsRef<Path>) -> Result<(), Box<dyn std::error::Error>> {
             map.insert(filename, BASE64_STANDARD.encode(buf));
         }
     }
+
+    let json = serde_json::to_string(&map)?;
+    let size_after = json.as_bytes().len();
+    compare_size(size_before, size_after);
 
     let timestamp = time::SystemTime::now()
         .duration_since(time::UNIX_EPOCH)?
@@ -67,8 +80,14 @@ fn read_dir(dir: impl AsRef<Path>) -> Result<(), Box<dyn std::error::Error>> {
         .to_string_lossy()
         .to_string();
     let out_file_path = format!("{dirname}-{timestamp:?}.json");
-    let writer = BufWriter::new(std::fs::File::create(out_file_path)?);
-    serde_json::to_writer_pretty(writer, &map)?;
+
+    let mut writer = BufWriter::new(std::fs::File::create(out_file_path)?);
+    writer.write_all(&json.as_bytes()[..])?;
 
     Ok(())
+}
+
+fn compare_size(before: usize, after: usize) {
+    let p = after as f32 / before as f32 * 100.0;
+    println!("\nsize: {before} -> {after} ( {p:.2}% )\n");
 }
